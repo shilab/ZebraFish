d6a01546b02bb188d004a31306b1c30d1a1a67ba
Lu Tian <ltian@Lus-MacBook-Air.local>
2014-05-21 11:38:25 -0400

    creat expression.pl

---
+++ expression.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl                                                                                                                                                                                             
+use strict;
+use warnings;
+
+my $kid_output;
+my $liver_output;
+my @kidney=(0);
+my @liver=(0);
+my $i=0;
+my @id;
+
+my $filename="gene_all_filesRMA-GENE-FULL-Group1.txt";
+open(FILE,$filename) || die "can't open that $filename";
+while (<FILE>)
+{
+    chomp;
+    if ($_ =~ /^Probe/)
+    {
+	my @header = split "\t", $_;
+	foreach (@header)
+	{
+	    my @temp = split(/ /, $_);
+	    push (@id, $temp[0]);
+	    if ($_ =~ /kid/)
+	    {
+		push(@kidney, $i);
+	    }
+	    elsif ($_ =~ /liver/)
+	    {
+		push(@liver, $i);
+	    }
+	    $i++;
+	}
+#	print "$_\n";
+    }
+    else
+    {
+	my @line = split "\t", $_;
+	$kid_output .= join ("\t", @line[@kidney]) ."\n";
+	$liver_output .= join ("\t", @line[@liver])."\n";
+#	print "$_\n";
+    }
+}
+close FILE;
+
+$id[0] = "id";
+$kid_output = join("\t", @id[@kidney]) ."\n$kid_output";
+$liver_output = join("\t", @id[@liver]) ."\n$liver_output";
+
+open FILE, ">". "kidney_expression" or die $!;
+print FILE $kid_output;
+close FILE;
+
+open FILE, ">". "liver_expression" or die $!;
+print FILE $liver_output;
+close FILE;
