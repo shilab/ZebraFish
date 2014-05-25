4f387939279b54cc2f35aac6404ceb2a33a4f972
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2014-05-25 10:33:31 -0400

    Cleaned up kid_sd.r

--- kid_sd.r
+++ kid_sd.r
@@ -18,7 +18,6 @@ mean <- mean(kid_sd,na.rm=T)
 length<-length(kid_sd)
 
 zscores <- vector(length=length(kid_sd))
-#create loop for kid_Means 
 
 for(i in 1:length)
 {
@@ -26,7 +25,6 @@ for(i in 1:length)
   zscores[i] <- z
 }
 
-#geneid<-geneid[which(zscores>3)]
 pvals<-pnorm(zscores,lower.tail=F)
 kid_expression.filt<-kidney_expression[which(pvals<.01),]
 write.table(kid_expression.filt,file="~/Development/repos/Zebrafish/kidney_expression.out.filter",sep="\t",row.names=FALSE,col.names=TRUE,quote=FALSE)
