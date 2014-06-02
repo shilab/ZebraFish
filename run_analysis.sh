938d33c7888d246935045449ea00fac26a83d959
Andrew Quitadamo <aquitada@uncc.edu>
2014-06-01 22:53:23 -0400

    Added updates to terminal

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,10 +1,13 @@
+echo "Extracting genotypes, expression and positions";
 perl cnv.pl
 perl cnv_pos.pl
 perl expression.pl
 perl ZebGene.pl
+echo "Overlapping and filtering";
 perl overlap.pl
 perl filter.pl CVN_matrix.out
 
+echo "Running analysis";
 R --no-save <<RSCRIPT
 library("MatrixEQTL", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 source('~/Development/repos/ZebraFish/kid_sd.r')
