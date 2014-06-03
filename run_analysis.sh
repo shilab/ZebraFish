e52aa34391c9c1f44c3b8bb083a6dbc5f8e23531
Andrew Quitadamo <aquitada@uncc.edu>
2014-06-03 13:12:01 -0400

    Added command line options to bash script

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,3 +1,29 @@
+usage()
+{ 
+	echo "Usage: $0 <-m MAF % filter> <-s p-value filter>" 1>&2; 
+	exit 1;
+}
+
+while getopts ":m:s:" o; do
+	case "${o}" in
+		m)
+			maf=${OPTARG}
+			;;
+		s)
+			sd=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
+if [ $OPTIND -ne 5 ]
+then
+	usage; 
+	exit 1; 
+fi
+
 echo "Extracting genotypes, expression and positions";
 perl cnv.pl
 perl cnv_pos.pl
@@ -5,13 +31,13 @@ perl expression.pl
 perl ZebGene.pl
 echo "Overlapping and filtering";
 perl overlap.pl
-perl filter.pl CNV_matrix.out 0.1
+perl filter.pl CNV_matrix.out ${maf}
 
 echo "Running analysis";
 R --no-save <<RSCRIPT
 library("MatrixEQTL", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 source('~/Development/repos/ZebraFish/expression_sd_filter.r')
-liv_expr<-expression_filter(0.01,'~/Development/Repos/ZebraFish/liver_expression.out')
-kid_expr<-expression_filter(0.01,'~/Development/Repos/ZebraFish/kidney_expression.out')
+liv_expr<-expression_filter(${sd},'~/Development/Repos/ZebraFish/liver_expression.out')
+kid_expr<-expression_filter(${sd},'~/Development/Repos/ZebraFish/kidney_expression.out')
 source('~/Development/repos/ZebraFish/zeb_meQTL.R')
 
