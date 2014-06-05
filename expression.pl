a1aa92b8abb0611d19a39018e9b9cfdbd01ac055
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2014-06-05 09:25:14 -0400

    Added documentation to expression.pl

--- expression.pl
+++ expression.pl
@@ -4,6 +4,7 @@ use warnings;
 
 my $kid_output;
 my $liver_output;
+#Initialize kidney and liver arrays with the ID column
 my @kidney=(0);
 my @liver=(0);
 my $i=0;
@@ -13,19 +14,25 @@ my $filename="gene_all_filesRMA-GENE-FULL-Group1.txt";
 open(FILE,$filename) || die "can't open that $filename";
 while (<FILE>)
 {
+    #Remove windows style newlines
     $_ =~ s/\r?\n$//;
     chomp;
     if ($_ =~ /^Probe/)
     {
+    	#Get columns by splitting by tabs
 	my @header = split("\t", $_);
 	foreach (@header)
 	{
+	    #Remove the trailing information from the id
 	    my @temp = split(/ /, $_);
+	    #Put id into the id array
 	    push (@id, $temp[0]);
+	    #Put kidney column numbers into the kidney array
 	    if ($_ =~ /kid/)
 	    {
 		push(@kidney, $i);
 	    }
+	    #Put the liver column numbers into the liver array
 	    elsif ($_ =~ /liver/)
 	    {
 		push(@liver, $i);
@@ -35,6 +42,7 @@ while (<FILE>)
     }
     elsif ($_=~/^[A-Za-z0-9]/)
     {
+    	#Split the lines and then rejoin using the column numbers in the kidney and liver arrays
 	my @line = split("\t", $_);
 	$kid_output .= join ("\t", @line[@kidney]) ."\n";
 	$liver_output .= join ("\t", @line[@liver])."\n";
@@ -42,6 +50,7 @@ while (<FILE>)
 }
 close FILE;
 
+#Get the ids for the header, and add the header to the beginning of the output strings
 $id[0] = "id";
 $kid_output = join("\t", @id[@kidney]) ."\n$kid_output";
 $liver_output = join("\t", @id[@liver]) ."\n$liver_output";
