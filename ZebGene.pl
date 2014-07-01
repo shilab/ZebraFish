04360ab0c3eded3629ea4871f6837cd835003817
Andrew Quitadamo <aquitada@uncc.edu>
2014-07-01 09:31:30 -0400

    Refactored ZebGene.pl for testing

--- ZebGene.pl
+++ ZebGene.pl
@@ -8,27 +8,34 @@ my $filename = "ZebGene-1_1-st-v1.r4.f38.design.transcript.design-time.pre-relea
 open(FILE, $filename) || die "Can't open file $filename";
 while (<FILE>)
 {
-	chomp;
-	if ($_=~/^\"\d/)
-	{
-		my @line = split(",",$_);
-		#Remove the quotes from the data
-		my $probeid = substr($line[1],1,-1);
-		my $chr = substr($line[2],1,-1);
-		my $start = substr($line[4],1,-1);
-		my $stop = substr($line[5],1,-1);
-
-		#Remove the chr from the chromosome data
-		if ($chr =~/^chr\d/)
-		{
-			$chr = substr($chr, 3);
-			#Add the data to the output
-			$output.= "$probeid\t$chr\t$start\t$stop\n";
-		}
-	}
+	$output.=parse($_);
 }
 
 #Print output to file
 open FILE,">"."gene_position" or die $!;
 print FILE $output;
 close FILE;
+
+sub parse
+{
+	$_=shift(@_);
+	chomp;
+	if ($_=~/^\"\d/)
+	{
+	    my @line = split(",",$_);
+	    #Remove the quotes from the data
+	    my $probeid = substr($line[1],1,-1);
+	    my $chr = substr($line[2],1,-1);
+	    my $start = substr($line[4],1,-1);
+	    my $stop = substr($line[5],1,-1);
+	    
+	    #Remove the chr from the chromosome data
+	    if ($chr =~/^chr\d/)
+	    {
+		$chr = substr($chr, 3);
+		#Add the data to the output
+		my $line = "$probeid\t$chr\t$start\t$stop\n";
+		return $line;
+	    }
+	}
+}
