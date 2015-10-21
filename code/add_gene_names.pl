1cfe9b782a41aae80b4203cca8757751563e2f9f
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-10-21 11:55:58 -0400

    Updated scripts to ids and names

--- code/add_gene_names.pl
+++ code/add_gene_names.pl
@@ -1,57 +1,46 @@
-#!/usr/bin/perl
 use strict;
 use warnings;
 
-my %id_annot;
+my %name_annot;
 my $file = shift(@ARGV);
 my $annot_file = "ZebGene-1_1-st-v1.r4.f38.design.transcript.design-time.pre-release.20110822.csv";
 
 open(FILE,$annot_file) || die "Can't open file $annot_file";
 while (<FILE>)
 {
-	chomp;
-	if ($_ =~ /^#/ || $_ =~ /^"transcript/)
+    chomp;
+	if ($_ =~ /protein_coding/)
 	{
-		next;
-	}
-	else
-	{
-		my @temp = split(",",$_);
-		my $id = $temp[0];
+		$_ = (split 'protein_coding')[0];
+		my @annot = split /\|/, $_;
+		my $genename = $annot[-1];
+		my $id = (split ",", $annot[0])[0];
 		$id =~ s/"//g;
-		my $annot = $temp[7];
-		my @annottemp = split(/\|/,$annot);
-		foreach(@annottemp)
-		{
-			if ($_ =~/^ENSDARG/)
-			{
-				$id_annot{$id}=$_;
-			}
-		}
+		$name_annot{$id}=$genename;
 	}
 }
+close FILE;
 
 open(FILE,$file) || die "Can't open file $file";
 while (<FILE>)
 {
-	if ($_ =~ /^SNP/)
-	{
-		print;
-	}
-	else
-	{
-		my @temp = split("\t",$_);
-		my $first = shift @temp;
-		my $id = shift @temp;
-		my $rest = join("\t", @temp);
-		if (exists $id_annot{$id})
-		{
-			print "$first\t$id\t$id_annot{$id}\t$rest";
-		}
-		else
-		{
-			print "$first\t$id\tNA\t$rest";
-		}
-	}
+    if ($_ =~ /^SNP/)
+    {
+        print;
+    }
+    else
+    {
+        my @temp = split("\t",$_);
+        my $first = shift @temp;
+        my $id = shift @temp;
+        my $rest = join("\t", @temp);
+        if (exists $name_annot{$id})
+        {
+            print "$first\t$id\t$name_annot{$id}\t$rest";
+        }
+        else
+        {
+            print "$first\t$id\tNA\t$rest";
+        }
+    }
 }
-
